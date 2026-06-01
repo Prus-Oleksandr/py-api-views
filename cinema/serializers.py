842e8bdca520ea557e8e5249772d729902a3652c
Prus Oleksandr <cheepaxxa2003@gmail.com>
2026-06-01 12:48:54 +0300

    Fix misnaming

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -91,7 +91,7 @@ class CinemaHallSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
     rows = serializers.IntegerField()
-    seats_in_rows = serializers.IntegerField()
+    seats_in_row = serializers.IntegerField()
 
     def create(self, validated_data):
         return CinemaHall.objects.create(**validated_data)
@@ -99,8 +99,8 @@ class CinemaHallSerializer(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.name = validated_data.get("name", instance.name)
         instance.rows = validated_data.get("rows", instance.rows)
-        instance.seats_in_rows = validated_data.get(
-            "seats_in_rows",
+        instance.seats_in_row = validated_data.get(
+            "seats_in_row",
             instance.seats_in_rows
         )
         instance.save()
